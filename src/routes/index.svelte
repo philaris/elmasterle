40661cfe4a94f0860a48c80c9eeb6e27994d7517
Panagiotis Cheilaris <philaris@cs.ntua.gr>
2022-02-23 12:21:44 +0100

    create initial page with languages

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,20 @@
 <script context="module" lang="ts">
-	export const prerender = true;
+    export const prerender = true;
 </script>
 
 <script lang="ts">
-	import Game from "$lib/Game.svelte"
+    import { Container, Row } from 'sveltestrap'
 </script>
 
 <svelte:head>
-	<title>elmasterle</title>
+    <title>elmasterle</title>
 </svelte:head>
 
-<Game language='el'/>
+<Container>
+    <Row><h1> Γουόρντλι </h1></Row>
+    <Row><a href="/el">el</a></Row>
+    <Row><a href="/en">en</a></Row>
+    <Row><a href="/hu">hu</a></Row>
+    <Row><a href="/it">it</a></Row>
+    <Row><a href= "https://github.com/philaris/elmasterle">Κώδικας</a></Row>
+</Container>
